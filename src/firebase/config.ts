a0b5875a391c77a7ab4d8bfa112af5781576dfbf
System Fabric <systemfabriccontato@gmail.com>
2026-02-09 17:45:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6529877098-afd61",
+  "appId": "1:390721738002:web:7950bf1890f35d2345cfcb",
+  "apiKey": "AIzaSyBgTdfx0HzPq2PT5DfHDJ34taLB0pY86zc",
+  "authDomain": "studio-6529877098-afd61.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "390721738002"
+};
