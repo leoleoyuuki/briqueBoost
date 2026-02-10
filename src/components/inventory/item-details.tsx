c3ec08f818e32899516df36af3f966dde8fcaefd
System Fabric <systemfabriccontato@gmail.com>
2026-02-10 13:10:06 +0000

    mas nao faz sentido isso quevoce falou, registrei compra a 800 e vendi o

--- src/components/inventory/item-details.tsx
+++ src/components/inventory/item-details.tsx
@@ -68,7 +68,7 @@ export function ItemDetails({ item }: { item: WithId<Item> }) {
 
       const itemRef = doc(firestore, 'users', user.uid, 'items', item.id);
       const userRef = doc(firestore, 'users', user.uid);
-      const profit = parseFloat((price - item.purchasePrice).toFixed(2));
+      const profit = Math.round((price - item.purchasePrice) * 100) / 100;
 
       const updatedItemData = {
           status: 'Sold' as const,
