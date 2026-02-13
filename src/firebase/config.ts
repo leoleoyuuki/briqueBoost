75648b37d9bbc4e59ba0289f65ac01b5c27d23b1
System Fabric <systemfabriccontato@gmail.com>
2026-02-13 13:34:04 +0000

    precisamos por as chaves do firestore no .env por mais segurança

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6529877098-afd61",
-  "appId": "1:390721738002:web:7950bf1890f35d2345cfcb",
-  "apiKey": "AIzaSyBgTdfx0HzPq2PT5DfHDJ34taLB0pY86zc",
-  "authDomain": "studio-6529877098-afd61.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "390721738002"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
